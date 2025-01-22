cd098d24a39ebd739f3569b39a0a14837e021733
Rodrigo Rocha <78671937+Rodrigo2603@users.noreply.github.com>
2025-01-22 20:25:09 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import Operacoes
 import numpy as np
 
 def teste(imagem1, kernel, cx, cy):
-    ImgOriginal = utils.LerImagem("../imagens/{}.jpg".format(imagem1))
+    ImgOriginal = utils.LerImagem("./imagens/{}.jpg".format(imagem1))
     ImgOriginal = utils.Binarizar(ImgOriginal)
 
     fig, [ax1,ax2] = plt.subplots(1,2)
@@ -14,115 +14,9 @@ def teste(imagem1, kernel, cx, cy):
     ax2.imshow(imagem,cmap='gray')
     plt.show()
 
-def mainOpera(imagem1,imagem2,operacao):
-    # formato das imagens 1a e 1b são diferentes do esperado 
-    ImgOriginal1 = utils.LerImagem('../imagens/{}.png'.format(imagem1))
-    ImgOriginal2 = utils.LerImagem('../imagens/{}.png'.format(imagem2))   
-
-    ImgResultado = utils.operation(ImgOriginal1,ImgOriginal2,operacao)
-
-    fig, [ax1,ax2,ax3] = plt.subplots(1,3,figsize=(10,5))
-    ax1.imshow(ImgOriginal1,cmap='gray')
-    ax2.imshow(ImgOriginal2,cmap='gray')
-    ax3.imshow(ImgResultado,cmap='gray')
-    plt.show()
-
-def filtro_de_ruido(imagem,SE,centrox,centroy):
-    ImgOriginal = utils.LerImagem("../imagens/{}.jpg".format(imagem))
-    ImgOriginal = utils.Binarizar(ImgOriginal)
-    
-    fig, [ax1,ax2] = plt.subplots(1,2)
-    ax1.imshow(ImgOriginal,cmap='gray')
-
-    imagem = Operacoes.Opening(ImgOriginal,kernel,centrox,centroy)
-    imagem = Operacoes.Closing(imagem,kernel,centrox,centroy)
-    
-    ax2.imshow(imagem,cmap='gray')
-    plt.show()
-
-def Preenche_furos(imagem,SE,centrox,centroy):
-    ImgOriginal = utils.LerImagem("../imagens/{}.jpg".format(imagem))
-    fig, [ax1,ax2] = plt.subplots(1,2)
-    ax1.imshow(ImgOriginal,cmap='gray')
-    ImgOriginal = utils.Binarizar(ImgOriginal)
-
-    imagem = Operacoes.Dilation(ImgOriginal,kernel,centrox,centroy)
-    imagem = Operacoes.Dilation(imagem,kernel,centrox,centroy)
-
-    imagem = Operacoes.Preencher_furos(imagem,kernel,centrox,centroy)
-    
-    imagem = Operacoes.Erosion(imagem,kernel,centrox,centroy)
-    # imagem = Operacoes.Erosion(imagem,kernel,centrox,centroy)
-    
-    ax2.imshow(imagem,cmap='gray')
-    plt.show()
-    
-def Quarta_Questao(imagem,SE,centrox,centroy):
-    ImgOriginal = utils.LerImagem("../imagens/{}.jpg".format(imagem))
-    ImgOriginal = utils.Binarizar(ImgOriginal)
-    fig, [ax1,ax2] = plt.subplots(1,2)
-    ax1.imshow(ImgOriginal,cmap='gray')
-
-    imagemD = Operacoes.Dilation(ImgOriginal,SE,1,1)
-    imagemD = Operacoes.Dilation(imagemD,SE,1,1)
-    imagemD = Operacoes.Dilation(imagemD,SE,1,1)   
-
-    imagem = Operacoes.Preencher_furos(imagemD,SE,1,1)
-
-    imagem = Operacoes.Erosion(imagem,SE,1,1)
-    imagem = Operacoes.Erosion(imagem,SE,1,1)
-    imagem = Operacoes.Erosion(imagem,SE,1,1)
-
-    ax2.imshow(imagem,cmap='gray')
-    plt.show()
-
-def Quarta_Questao_separar(imagem,SE,centrox,centroy):
-    ImgOriginal = utils.LerImagem("../imagens/{}.jpg".format(imagem))
-    ImgOriginal = utils.Binarizar(ImgOriginal)
-    fig, [ax1,ax2] = plt.subplots(1,2)
-    ax1.imshow(ImgOriginal,cmap='gray')
-
-    imagemD = Operacoes.Dilation(ImgOriginal,SE,1,1)
-    imagemD = Operacoes.Dilation(imagemD,SE,1,1)
-    imagemD = Operacoes.Dilation(imagemD,SE,1,1)   
-
-    imagem = Operacoes.Preencher_furos(imagemD,SE,1,1)
-
-    imagem = Operacoes.Erosion(imagem,SE,1,1)
-    imagem = Operacoes.Erosion(imagem,SE,1,1)
-    imagem = Operacoes.Erosion(imagem,SE,1,1)
-
-    erodi = Operacoes.Erosion(ImgOriginal,SE,1,1)
-    # erodi = Operacoes.Erosion(erodi,SE,1,1)
-    # erodi = Operacoes.Erosion(erodi,SE,1,1)
-    # erodi = Operacoes.Erosion(erodi,SE,1,1)
-
-    subImg = np.array(imagem) - np.array(erodi)
-    # subImg = ImgOriginal - subImg
-
-    separa = utils.operation(subImg,ImgOriginal,'xor')
-
-    ax2.imshow(separa,cmap='gray')
-    plt.show()
-
-
 if __name__ == '__main__':
     kernel = [[0,1,0],[1,1,1],[0,1,0]]
     kernel1 = [[0,1,0],[1,1,1],[0,0,0]]
     kernel2 = [[1,1,1],[1,1,1],[1,1,1]]
 
-    # mainOpera('Image_(1a)','Image_(1b)','nand')
-
-    #filtro_de_ruido('Image_(2a)',kernel,1,1)
-
-    teste('teste3', kernel2, 1, 1)
-
-    # Preenche_furos('Image_(3a)',kernel,1,1)
-
-    # Quarta_Questao('Image_(4a)',kernel,1,1)
-
-    #Quarta_Questao_separar('Image_(4a)',kernel,1,1)
-    
-    # mainPreenche('Image_(3a)')
-
-    # plt.savefig('../resultados/teste_{}_{}_{}_{}x{}.png'.format(tipo,imagem,filtro,m,n),dpi=150,bbox_inches='tight')
+    teste('Image_(2a)', kernel2, 1, 1)
