04983174f4c51941748d16343fcb83223c0f417a
Rodrigo Rocha <78671937+Rodrigo2603@users.noreply.github.com>
2025-01-22 21:17:16 -0300

    Add files via upload

--- Operacoes.py
+++ Operacoes.py
@@ -1,75 +1,65 @@
-import utils
-import numpy as np
-import matplotlib.pyplot as plt
-
 def Erosion(imagem, SE, centrox, centroy):
-    aux = np.shape(imagem)
+    linhas = len(imagem)
+    colunas = len(imagem[0])
 
-    if np.size(aux) > 2:
-        imagem = imagem[:,:,0] # seleciona apenas uma matriz de cor caso a leitura seja rgb
-        aux = np.shape(imagem)
+    # Se imagem for RGB (3D), pega apenas a matriz de cor
+    if isinstance(imagem[0][0], list):
+        imagem = [linha[0] for linha in imagem]
+        linhas = len(imagem)
+        colunas = len(imagem[0])
     
-    for x in range(aux[0]): # cria-se o complemento da imagem
-        for y in range(aux[1]):
+    # Cria o complemento da imagem
+    for x in range(linhas):
+        for y in range(colunas):
             imagem[x][y] = 1 - imagem[x][y]
 
-    ImgDilat = Dilation(imagem, SE, centrox, centroy) # erode o complemento da imagem
+    ImgDilat = Dilation(imagem, SE, centrox, centroy)  # Dilata o complemento
 
-    for x in range(aux[0]): # tira-se o complemento da imagem erodida
-        for y in range(aux[1]):
+    # Tira o complemento da imagem erodida
+    for x in range(linhas):
+        for y in range(colunas):
             ImgDilat[x][y] = 1 - ImgDilat[x][y]
 
-    # corrige os valores das bordas
+    # Corrige valores das bordas
+    for i in range(linhas):
+        ImgDilat[i][0] = ImgDilat[i][1]
+        ImgDilat[i][-1] = ImgDilat[i][-2]
 
-    ImgDilat[:][-1] = ImgDilat[:][-2] 
-    ImgDilat[:][0] = ImgDilat[:][1]
-    ImgDilat[0][:] = ImgDilat[1][:]
-    ImgDilat[-1][:] = ImgDilat[-2][:] 
+    for j in range(colunas):
+        ImgDilat[0][j] = ImgDilat[1][j]
+        ImgDilat[-1][j] = ImgDilat[-2][j]
 
     return ImgDilat
 
 def Dilation(imagem, SE, centrox, centroy):
-    aux = np.shape(imagem)
+    linhas = len(imagem)
+    colunas = len(imagem[0])
+    tam_SE = (len(SE), len(SE[0]))  # Tamanho do elemento estruturante
 
-    if np.size(aux) > 2:
-        imagem = imagem[:,:,0] # seleciona apenas uma matriz de cor caso a leitura seja rgb
-        aux = np.shape(imagem)
+    ImgErod = []  # Array para a imagem dilatada
+    check = 0  # Flag para indicar se o SE está contido
+    total = sum(SE[u][v] != 0 for u in range(tam_SE[0]) for v in range(tam_SE[1]))
 
-    ImgErod = [] # array vazio que será responsável pelas colunas da imagem
-    ImgLinha = [] # array vazio que será responsável pelas linhas da imagem
-    tam = np.shape(SE) # tamanho do elemento estruturante (SE)
-    check = 0 # flag para indicar se o SE está completamente contido no objeto
-    total = 0 # flag para indicar a quantidade de pixels 1 dentro do elemento estruturante
-
-    for u in range(tam[0]):
-        for v in range(tam[1]):
-            if SE[u][v] != 0:
-                total += 1 # contagem da quantidade de pixels maior que zero
-
-    for x in range(aux[0]):
-        for y in range(aux[1]):
-            if imagem[x][y] == 1: # verifica se o pixel sobre analise é branco
-                for u in range(0-centrox,tam[0]-centrox):
-                    for v in range(0-centroy,tam[1]-centroy):
-                        if x+u >= 0 and x+u <aux[0] and y+v >= 0 and y+v < aux[1]: 
-                            # incrementa o flag se o SE está td contido no objeto
-                            check += SE[u+centrox][v+centroy]*imagem[x+u][y+v] 
-                if check == total: # se o SE está td contido no objeto da imagem
-                    ImgLinha.append(1) # adiciona-se um pixel igual a 1
-                else: # caso contrário, adiciona-se um pixel igual a 0
+    for x in range(linhas):
+        ImgLinha = []
+        for y in range(colunas):
+            if imagem[x][y] == 1:  # Verifica se o pixel é branco
+                for u in range(-centrox, tam_SE[0] - centrox):
+                    for v in range(-centroy, tam_SE[1] - centroy):
+                        if 0 <= x + u < linhas and 0 <= y + v < colunas:
+                            check += SE[u + centrox][v + centroy] * imagem[x + u][y + v]
+                if check == total:
+                    ImgLinha.append(1)
+                else:
                     ImgLinha.append(0)
                 check = 0
             else:
                 ImgLinha.append(0)
-        ImgErod.append(ImgLinha) # se adiciona-se uma nova coluna no array
-        ImgLinha = []
-
+        ImgErod.append(ImgLinha)
     return ImgErod
 
-def Opening(imagem,SE,centrox,centroy):
-    ImgOpen = Dilation(Erosion(imagem,SE,centrox,centroy),SE,centrox,centroy)
-    return ImgOpen
+def Opening(imagem, SE, centrox, centroy):
+    return Dilation(Erosion(imagem, SE, centrox, centroy), SE, centrox, centroy)
 
-def Closing(imagem,SE,centrox,centroy):
-    ImgClose = Erosion(Dilation(imagem,SE,centrox,centroy),SE,centrox,centroy)
-    return ImgClose
+def Closing(imagem, SE, centrox, centroy):
+    return Erosion(Dilation(imagem, SE, centrox, centroy), SE, centrox, centroy)
